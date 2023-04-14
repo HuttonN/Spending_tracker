5c0783a438d4a7601d60255432cadc6584a40244
justneil11 <justneil11@gmail.com>
2023-04-14 15:18:34 +0100

    Changes made to sql file after tests

--- db/spending_manager.sql
+++ db/spending_manager.sql
@@ -1,18 +1,20 @@
 DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS merchants;
 
 CREATE TABLE merchants (
     id SERIAL PRIMARY KEY,
-    name VARCHAR (255),
+    name VARCHAR (255)
 );
 
 CREATE TABLE tags (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     name VARCHAR (255)
-)
+);
 
 CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY.
-    amount DECIMAL (6,2)
-    merchant_id INT NOT NULL REFERENCES merchants(id)
+    id SERIAL PRIMARY KEY,
+    amount DECIMAL (6,2),
+    merchant_id INT NOT NULL REFERENCES merchants(id),
+    tag_id INT NOT NULL REFERENCES tags(id)
 );
